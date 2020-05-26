c51cf6b0ec471a280827b98781462b212a9cfbe2
obedondimu <ondimuobed@gmail.com>
2020-05-26 12:23:58 +0300

    Complete Django Project

--- blogs/blogs/settings.py
+++ blogs/blogs/settings.py
@@ -134,3 +134,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL= 'newblog-home'
 
 LOGIN_URL = 'login'
+
+#EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+#EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_POST = 587
+EMAIL_USE_TLS = True 
+EMAIL_HOST_USER =  os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
--- blogs/users/models.py
+++ blogs/users/models.py
@@ -15,8 +15,8 @@ class Profile(models.Model):
 		
 		img =Image.open(self.image.path)
 
-		if img.height > 300 or img.width > 300:
-			output_size = (300,300)
+		if img.height > 100 or img.width > 100:
+			output_size = (100,100)
 			img.thumbnail(output_size)
 			img.save(self.image.path)
  				
